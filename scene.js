b38347f9609db20371abfcb3a47211eb474452e1
ssuarez-temp <ts5323@nyu.edu>
2026-03-13 03:42:54 -0400

    Final major update

--- scene.js
+++ scene.js
@@ -4,11 +4,60 @@ let Rects = [];
 let elimQueue = [];
 let curr = null;
 let lastReplaced = 0;
-let interval = 1000;
+let elimInterval = 750; // eliminations by person
+let targetInterval = 750; // scanning
+let destroyInterval = 500; // 
+let shatterInterval = 500; // 
+let replaceInterval = 1000; // 
+
+let triggeredScenes = { // scene control mapping
+  "Communists": false,
+  "Socialists": false,
+  "Trade Unionists": false,
+  "none": false,
+  "Me": false
+};
+let activeScene = null; // scene control
+
+class Particle {
+  constructor(x, y, c = color(255)) {
+    this.x = x;
+    this.y = y;
+    this.vx = random(-1.5, 1.5);
+    this.vy = random(-0.4, 2);
+    this.life = 255;
+    this.r = red(c);
+    this.g = green(c);
+    this.b = blue(c);
+  }
+  update() {
+    this.x += this.vx;
+    this.y += this.vy;
+    this.life -= 8;
+  }
+  draw(r = 3) {
+    fill(this.r, this.g, this.b, this.life);
+    ellipse(this.x, this.y, r);
+  }
+  dead() {
+    return this.life <= 0;
+  }
+}
+
+class scanParticle extends Particle {
+  constructor(x, y) {
+    super(x, y, color(255), 2);
+    this.vx = 0;
+    this.vy = random(-1.5, 0);
+  }
+  update() {
+    super.update();
+    // this.life -= 5;
+  }
+}
 
 class Person {
-
-  constructor(x, y, r, c = color(random(255), random(255), random(255)), group = null) {
+  constructor(x, y, r, c = color(random(255), random(255), random(255)), group = "none") {
     // position
     this.x = x;
     this.y = y;
@@ -27,21 +76,66 @@ class Person {
     // scanned
     this.group = group;
     this.scanned = false;
+    this.status = "secure" // "secure" "target" "destroy" "replaced"
+    this.timer = 0; // timer for replacement
+    this.destroyR = r; // radius for shimmering in destruction
+    this.flash = true; // flashing for shimmering
+    // particles
+    this.fragments = [];
+    this.fade = 0;
+    this.destroyStarted = false;
   }
   shimmer() { // shimmering
     this.x += random(-1, 1);
     this.y += random(-1, 1);
   }
-  march(speed) {
+  march(speed) { // unused marching
     this.y += speed;
   }
   changeGroup(newGroup, newColor) {
     this.group = newGroup;
     this.c = newColor;
   }
-  replaced() {
-    this.scanned = true;
-    this.changeGroup(null, color(255));
+  replaced() { // start of elimination process
+    if (this.status !== "secure") return;
+    this.status = "target";
+    this.timer = millis();
+    // this.changeGroup(null, color(255));
+  }
+  update() { // update position and status
+    if (this.status === "target") {
+      if (millis() - this.timer >= targetInterval) {
+        this.status = "destroy";
+        this.timer = millis();
+      }
+    }
+    else if (this.status === "destroy") {
+      let t = millis() - this.timer;
+      this.destroyR = this.group === "Me" ?
+        this.r : map(t, 0, destroyInterval, this.r, this.r * 1.05);
+      if (millis() - this.timer >= destroyInterval && this.group !== "Me") {
+        this.status = "shatter";
+        this.timer = millis();
+      }
+    }
+    else if (this.status === "shatter") {
+      if (!this.destroyStarted) { // push in particles
+        for (let i = 0; i < 10; i++) {
+          this.fragments.push(new Particle(this.x, this.y, this.c));
+        }
+        this.destroyStarted = true;
+      }
+      if (millis() - this.timer >= shatterInterval && this.group !== "none") {
+        this.status = "replaced";
+        this.timer = millis();
+        this.c = color(255);
+      }
+    } else if (this.status === "replaced") {
+      if (millis() - this.timer >= replaceInterval) {
+        this.status = "replaced";
+        this.timer = millis();
+      }
+    }
   }
   draw(opacity) {
     if (opacity !== undefined) {
@@ -49,8 +143,66 @@ class Person {
     } else {
       fill(this.c);
     }
-    ellipse(this.x, this.y, this.r * 2, this.r * 2);
+    if (this.status === "secure") {
+      ellipse(this.x, this.y, this.r * 2, this.r * 2);
+    } else if (this.status === "target") {
+      ellipse(this.x, this.y, this.r * 2, this.r * 2);
+      stroke(255);
+      strokeWeight(2);
+      let t = millis() - this.timer;
+      let d = map(t, 0, targetInterval, this.r * 3, this.r * 2);
+      // scanning frame
+      line(this.x - d, this.y - d, this.x - 0.5 * d, this.y - d);
+      line(this.x - d, this.y - d, this.x - d, this.y - 0.5 * d);
+
+      line(this.x + d, this.y - d, this.x + 0.5 * d, this.y - d);
+      line(this.x + d, this.y - d, this.x + d, this.y - 0.5 * d);
+
+      line(this.x - d, this.y + d, this.x - 0.5 * d, this.y + d);
+      line(this.x - d, this.y + d, this.x - d, this.y + 0.5 * d);
+
+      line(this.x + d, this.y + d, this.x + 0.5 * d, this.y + d);
+      line(this.x + d, this.y + d, this.x + d, this.y + 0.5 * d);
+      noStroke();
+    } else if (this.status === "destroy") {
+      this.flash = this.group === "Me" ?
+        frameCount % 80 < 40 : frameCount % 6 < 3;
+      if (this.flash) {
+        stroke(255);
+        strokeWeight(2);
+        let d = this.destroyR * 2;
+        line(this.x - d, this.y - d, this.x - 0.5 * d, this.y - d);
+        line(this.x - d, this.y - d, this.x - d, this.y - 0.5 * d);
+
+        line(this.x + d, this.y - d, this.x + 0.5 * d, this.y - d);
+        line(this.x + d, this.y - d, this.x + d, this.y - 0.5 * d);
+
+        line(this.x - d, this.y + d, this.x - 0.5 * d, this.y + d);
+        line(this.x - d, this.y + d, this.x - d, this.y + 0.5 * d);
+
+        line(this.x + d, this.y + d, this.x + 0.5 * d, this.y + d);
+        line(this.x + d, this.y + d, this.x + d, this.y + 0.5 * d);
+        noStroke();
+      }
+      if (this.group !== "Me") this.shimmer();
+      ellipse(this.x, this.y, this.destroyR * 2, this.destroyR * 2);
+    }
+    else if (this.status === "shatter") {
+      for (let i = this.fragments.length - 1; i >= 0; --i) {
+        let f = this.fragments[i];
+        f.update();
+        f.draw(this.r / 1.25);
+        this.fragments = this.fragments.filter(f => !f.dead());
+      }
+    } else if (this.status === "replaced") {
+      this.fade += 5;
+      if (this.fade > 255) this.fade = 255;
+      fill(255, this.fade);
+      ellipse(this.x, this.y, this.r * 2, this.r * 2);
+    }
   }
+
+
 }
 
 class Rectangle {
@@ -97,18 +249,68 @@ function back() { // background rectangles, mimicing television static
   }
 }
 
+function drawTime() { // time for testing purposes
+
+  let ms = millis();
+
+  let cs = floor((ms % 1000) / 10);
+  let s = floor(ms / 1000) % 60;
+  let m = floor(ms / 60000) % 60;
+  let h = floor(ms / 3600000);
+
+  function pad(n) {
+    if (n < 100) {
+      return n.toString().padStart(2, "0");
+    }
+  }
+
+  let t = pad(h) + ":" + pad(m) + ":" + pad(s) + "." + pad(cs);
+
+  fill(255);
+  noStroke();
+  textSize(16);
+  textFont("monospace");
+  text(t, 10, 25);
+}
+
 function scene0(opacity) {
   cruz = new Cross(width / 2, height / 2, 100, 10);
   cruz.draw(opacity);
 }
 
+function drawScanParticles() {
+  let spacing = 5;
+  let cols = floor(width / spacing);
+  for (let i = 0; i < cols; i++) {
+    let x = i * spacing + spacing / 2;
+    let y = scanY;
+    scanParticles.push(new scanParticle(x, y));
+  }
+  for (let i = scanParticles.length - 1; i >= 0; i--) {
+    let p = scanParticles[i];
+    p.update();
+    p.draw(7.5);
+    if (p.dead()) {
+      scanParticles.splice(i, 1);
+    }
+  }
+}
+
 function killUpdate() {
-  if(elimQueue.length > 0 && curr === null && millis() - lastReplaced >= interval) {
+  for (let i = 0; i < grid.length; ++i) {
+    for (let j = 0; j < grid[i].length; ++j) {
+      let p = grid[i][j];
+      p.update();
+    }
+  }
+  if (elimQueue.length > 0 && curr === null
+    && millis() - lastReplaced >= elimInterval) {
     curr = elimQueue.shift(); // first-in-first-out
     curr.replaced();
+    // curr.scanned = false;
     lastReplaced = millis();
   }
-  if(curr !== null && curr.scanned == true) {
+  if (curr !== null && curr.scanned == true) {
     curr = null;
   }
 }
--- sketch.js
+++ sketch.js
@@ -1,8 +1,11 @@
 let col = 5, row = 5;
 
 let scanY = 0;        // scan line
-let scanSpeed = 1;
+let scanSpeed = 1.5;
 let scanHeight = 50;  // the "height" of the scan line
+
+let scanParticles = [];
+
 
 function setup() {
   noStroke();
@@ -41,18 +44,25 @@ function setup() {
   grid[0][1].changeGroup("Communists", color(255, 0, 0));
   grid[1][1].changeGroup("Communists", color(255, 0, 0));
   grid[1][2].changeGroup("Communists", color(255, 0, 0));
+  grid[2][1].changeGroup("Communists", color(255, 0, 0));
   grid[3][0].changeGroup("Communists", color(255, 0, 0));
   grid[4][0].changeGroup("Communists", color(255, 0, 0));
   //Then they came for the Socialists—
+  grid[2][3].changeGroup("Socialists", color(225, 21, 132));
+  grid[3][1].changeGroup("Socialists", color(225, 21, 132));
   grid[3][2].changeGroup("Socialists", color(225, 21, 132));
   grid[3][3].changeGroup("Socialists", color(225, 21, 132));
+  grid[4][1].changeGroup("Socialists", color(225, 21, 132));
   grid[4][2].changeGroup("Socialists", color(225, 21, 132));
   grid[4][3].changeGroup("Socialists", color(225, 21, 132));
   //Then they came for the Trade Unionists—
+  grid[0][0].changeGroup("Trade Unionists", color(255, 170, 0));
+  grid[0][3].changeGroup("Trade Unionists", color(255, 170, 0));
   grid[0][4].changeGroup("Trade Unionists", color(255, 170, 0));
+  grid[1][0].changeGroup("Trade Unionists", color(255, 170, 0));
+  grid[1][3].changeGroup("Trade Unionists", color(255, 170, 0));
   grid[1][4].changeGroup("Trade Unionists", color(255, 170, 0));
   grid[2][4].changeGroup("Trade Unionists", color(255, 170, 0));
-  grid[3][4].changeGroup("Trade Unionists", color(255, 170, 0));
   //Then they came for me—
   grid[2][2].changeGroup("Me", color(224, 224, 224));
 }
@@ -63,13 +73,69 @@ function draw() {
   // if (millis() < 15000) {
   //   scene0(map(millis(),0,15000,255,0));
   // }
+  drawTime();
   scene0(map(constrain(millis(), 0, 12500), 0, 12500, 255, 64)); // the cross
   if (millis() > 2500) {
     scene1();
   }
-  if (millis() > 10000) {
-    scene2("Communists");
+  if (millis() > 10000 && !triggeredScenes["Communists"]) {
+    activeScene = "Communists";
+    triggeredScenes["Communists"] = true;
+    scene2(activeScene, true);
+  } else if (activeScene === "Communists") {
+    scene2(activeScene);
   }
+  //And I did not speak out, because I was not a Communist;
+  if (millis() > 20000 && !triggeredScenes["Socialists"]) {
+    activeScene = "Socialists";
+    triggeredScenes["Socialists"] = true;
+    scene2(activeScene, true);
+  } else if (activeScene === "Socialists") {
+    scene2(activeScene);
+  }
+  //And I did not speak out, because I was not a Socialist;
+  if (millis() > 30000 && !triggeredScenes["Trade Unionists"]) {
+    activeScene = "Trade Unionists";
+    triggeredScenes["Trade Unionists"] = true;
+    scene2(activeScene, true);
+  } else if (activeScene === "Trade Unionists") {
+    scene2(activeScene);
+  }
+  //And I did not speak out, because I was not a Trade Unionist;
+  if (millis() > 42500 && !triggeredScenes["none"]) {
+    scanSpeed = 2.5;
+    elimInterval = 175;
+    for (let i = 0; i < col; ++i) {
+      for (let j = 0; j < row; ++j) {
+        let p = grid[i][j];
+        if (i != 2 || j != 2) {
+          p.scanned = false;
+          p.group = "none";
+          p.status = "secure"
+          p.fragments = [];
+          p.fade = 0;
+          p.destroyStarted = false;
+        }
+      }
+    }
+    activeScene = "none";
+    triggeredScenes["none"] = true;
+    scene2(activeScene, true);
+  } else if (activeScene === "none") {
+    scene2(activeScene);
+  }
+  //M A S S A C R E
+  if (millis() > 52500 && !triggeredScenes["Me"]) {
+    scanSpeed = 1;
+    targetInterval = 1250;
+    activeScene = "Me";
+    triggeredScenes["Me"] = true;
+    scene2(activeScene, true);
+  } else if (activeScene === "Me") {
+    scene2(activeScene);
+  }
+  //And there was no one left
+  //To speak out for me
 }
 
 function scene1() {
@@ -87,22 +153,27 @@ function scene1() {
   }
 }
 
-function scene2(group) {
-  killUpdate();
-  for (let i = 0; i > -scanHeight; --i) {
-    let opacity = map(i, 0, -scanHeight, 255, 0);
-    fill(255, opacity);
-    rect(0, scanY + i, width, 1);
+function scene2(group, resetScan = false) {
+  if (resetScan) {
+    scanY = 0;
   }
+  killUpdate();
+  // for (let i = 0; i > -scanHeight; --i) {
+  //   let opacity = map(i, 0, -scanHeight, 255, 0);
+  //   fill(255, opacity);
+  //   noStroke();
+  //   rect(0, scanY + i, width, 1);
+  // }
+  drawScanParticles();
   let scanTop = scanY;
   let scanBottom = scanY - scanHeight;
   for (let i = 0; i < col; ++i) {
     for (let j = 0; j < row; ++j) {
       let p = grid[i][j];
-      if (p.group === group && p.y <= scanTop && p.y >= scanBottom && !p.scanned) {
+      if (!p.scanned && p.group === activeScene
+        && p.y <= scanTop && p.y >= scanBottom) {
         elimQueue.push(p);
         p.scanned = true;
-        // p.replaced();
       }
     }
   }
